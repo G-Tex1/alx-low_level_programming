98913bf2c509acb16959b094ee843490afbce0ef
G-Tex1 <ghenorbari@gmail.com>
2022-12-08 14:20:17 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 #!/bin/bash
-puts(""Programming is like building a multilingual puzzle\n")
+#include<stdio>
+int main()
+{
+    puts(""Programming is
+    like building a multilingual
+    puzzle \n");
+    return (0);
+}
