54280e3964fbbe0e3450186157f27720d2b28fff
G-Tex1 <ghenorbari@gmail.com>
2022-12-14 04:53:16 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check code and  Prints lower case letters from a to z
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+	return (0);
+}
