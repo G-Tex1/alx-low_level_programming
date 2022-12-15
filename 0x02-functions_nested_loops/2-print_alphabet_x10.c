a0137cb03562c57b685333aa8c81d2466759dd3e
G-Tex1 <ghenorbari@gmail.com>
2022-12-15 09:14:10 -0800

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,23 @@
  * print_alphabet_x10 - prints 10x the alphabets followed by a new line
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10()
 {
-	char 'la';
+	char la;
+
+	for (la='a'; la<='z'; la++){
+		_putchar(la);
+	}
+}
+
+int main(void)
+{
 	int n;
 
-	for (n = 0; n < 10; n++)
-		for (la = 'a'; la <= 'z'; la++)
-			_putchar(la);
-
+	for (n=0; n<10; n++){
+		print_alphabet_x10();
 		_putchar('\n');
+	}
+
+	return (0);
 }
