77f11701b03131e32ca75534dbe791909b6b39d2
G-Tex1 <ghenorbari@gmail.com>
2022-12-15 14:49:03 -0800

    lower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,19 +3,13 @@
  * print_alphabet - Prints alphabets in lower case
  */
 
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char lc;
 
 	for (lc = 'a'; lc <= 'z'; lc++)
-	return lc;
-}
-
-int main(void)
-{
-	char lc;
-
-	_putchar(lc);
-	_putchar('\n');
-	return (0);
+	{
+		_putchar(lc);
+		_putchar('\n');
+	}
 }
