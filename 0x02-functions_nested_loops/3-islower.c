a98b2a868fce11192d91347770f5091d8d73fc20
G-Tex1 <ghenorbari@gmail.com>
2022-12-15 20:40:52 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - c is lower
+ * Return: Returns 1 if c is lower. Else it returns 0
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
