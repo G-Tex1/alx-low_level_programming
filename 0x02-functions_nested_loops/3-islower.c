63fc0a260d4ee987eaea2381161372a9367f5407
G-Tex1 <ghenorbari@gmail.com>
2022-12-15 21:46:26 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 /**
  * _islower - c is lower
  * Return: Returns 1 if c is lower. Else it returns 0
+ * 'c' : numeric value of lowercase alphabets
  */
-
-int _islower(int c)
+int _islower(void)
 {
+	int c;
 
 	if (c >= 97)
 	{
