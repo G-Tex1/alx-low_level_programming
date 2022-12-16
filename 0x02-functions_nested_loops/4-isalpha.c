9fc68c77cadca81ac01129cd48efa02e6c3a5a2b
G-Tex1 <ghenorbari@gmail.com>
2022-12-16 04:03:01 -0800

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - prints lower and upper case alphabets
+ * @c: Takes in the character
+ * Return: Returns 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65)
+	{
+		return (1);
+	}
+
+
+	else
+	{
+		return (0);
+	}
+}
