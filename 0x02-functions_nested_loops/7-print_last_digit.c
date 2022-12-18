449243f2ac49598af09e6bfb1b21c5005a8cda4d
G-Tex1 <ghenorbari@gmail.com>
2022-12-18 11:37:18 -0800

    dig

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Return: Returns the number
+ * @int: Contains value
+ */
+
+int print_last_digit(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		result = n % 10;
+		return (result);
+	}
+	else
+	{
+		result = n % 10;
+		return (result);
+	}
+}
