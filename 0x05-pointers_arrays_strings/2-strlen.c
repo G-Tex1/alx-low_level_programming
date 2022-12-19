d630a9eb92c4b5574377c11624c100edac3b30bd
G-Tex1 <ghenorbari@gmail.com>
2022-12-19 13:50:05 -0800

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - measures the length of string
+ * @s: imputed character to be measured
+ * Return: Returns the length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		*s++;
+		len++;
+	}
+	return (len);
+}
