e42c3e9b1977c1026b45ee33077a6668cb4044b8
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 10:30:02 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ * @str: contains the string to be printed
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
