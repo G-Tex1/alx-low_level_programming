7d625d93d125b4c681650d1beebea3c969644427
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 15:06:51 -0800

    execute

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,13 @@
 
 void times_table(void)
 {
-	int i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	int b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	int num1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int num2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int c;
 
 	while (c <= 9)
 	{
-		c = i * b;
+		c = num1 * num2;
 		_putchar(c);
 		_putchar(',');
 		_putchar('\n');
