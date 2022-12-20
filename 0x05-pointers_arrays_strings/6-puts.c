63a4a3fec3724e4a7bad9dcf223576809751085b
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 09:26:08 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str - contains the string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for ( str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
