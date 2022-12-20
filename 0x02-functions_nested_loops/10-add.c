70a86f9c9c540479dcf8b8d466d8461bf69de298
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 12:49:31 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: integer a stores the argument
+ * @b: integer b stores the second argument
+ * Return: Returns the result
+ */
+
+int add(int a, int b)
+{
+	int t;
+
+	t = a + b;
+	return (t);
+}
