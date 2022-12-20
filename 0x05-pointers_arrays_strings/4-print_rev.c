f67a353efc3bc3e0cebfd9e24135817f4c66d5c6
G-Tex1 <ghenorbari@gmail.com>
2022-12-19 19:11:53 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse
+ * @s: contains the argument
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1, i >= 0, i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
