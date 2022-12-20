1365c99c9132bd49e96172f2adda9b9696c9bae9
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 10:55:58 -0800

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - prints the values of an array seperated by comma
+ * @a: a pointer containing integers
+ * @n: another container holding n number of integers
+ */
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
+
+	printf('\n');
+}
