1cb1b78064dbadd030c57827a26cd0decacb333c
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 16:05:27 -0800

    execute

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - upper case letters
+ * @c: container for character
+ * Return: Returns 1  or 0
+ */
+int _isupper(int c)
+{
+	if (c < 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
