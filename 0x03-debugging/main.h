8d1159f3fe7c09c0382a5c435b48708e2dfef876
G-Tex1 <ghenorbari@gmail.com>
2022-12-20 21:17:29 -0800

    execute

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int positive_or_negative();
+int positive_or_negative(void);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
