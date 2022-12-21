ad3ce47b3b4747d9b009d7c8b15c132a65f227fa
G-Tex1 <ghenorbari@gmail.com>
2022-12-21 12:39:41 -0800

    cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - catinates the strings
+ * @dest: string destination
+ * @src: string source
+ * @n: the number where to be catinated
+ * Return: Return dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest[i]);
+
