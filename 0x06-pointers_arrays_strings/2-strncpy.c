91f2da22f72c4b9da440bf28f26fea3c39b07370
G-Tex1 <ghenorbari@gmail.com>
2022-12-22 03:38:26 -0800

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - catinates the strings
+ * @dest: destination to be copied to
+ * @src: source to copy from
+ * @n: number to copy from
+ * Return: Returns char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && dest[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
