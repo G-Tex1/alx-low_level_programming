133ef57bbfa6e1ebf42ce79ec5c6c455d96e9ffe
G-Tex1 <ghenorbari@gmail.com>
2022-12-22 08:23:57 -0800

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && dest[i] != '\0')
+	for (i < n; src[i]; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	dest[i] = '\0';
 	return (dest);
 }
