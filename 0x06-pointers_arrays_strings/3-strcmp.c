c543e5140b9e226bc466b010d37b3ca5e3521440
G-Tex1 <ghenorbari@gmail.com>
2022-12-22 09:14:25 -0800

    cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares strings
+ * @s1: hold string to be compared
+ * @s2: holds the second string
+ * Return: Returns the difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
