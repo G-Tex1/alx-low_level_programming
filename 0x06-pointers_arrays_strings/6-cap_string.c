367528d7260066c11702aabd4ad90526b9889c30
G-Tex1 <ghenorbari@gmail.com>
2022-12-22 13:34:00 -0800

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - Capitalises the string
+ * @n: stores the value inserted
+ * Return: Returns n
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 97 && n[i - 1] == ' ' || n[i - 1] == '\t' || n[i - 1] == '\n' || n[i - 1] == ',' || n[i - 1] == ';' || n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' || n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' || n[i - 1] == '{' || n[i - 1] == '}')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+
+	return (n);
+}
