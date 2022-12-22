fdc52f35375b1e0f6e6b6e3e06e8ac301771c1cb
G-Tex1 <ghenorbari@gmail.com>
2022-12-22 12:19:21 -0800

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - Converts to upper case
+ * @n: Holds the character
+ * Return: return n
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; n[i] != 0; i++)
+	{
+		if (n[i] <= 97)
+		{
+			n[i] = n[i] - 32;
+		}
+		n[i] = '\0';
+	}
+	return (n);
+}
