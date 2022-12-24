64ee74a77b557e8be99307727cd419e6fbedb77a
G-Tex1 <ghenorbari@gmail.com>
2022-12-24 09:03:01 -0800

    int

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints digits of base 10
+ * Return: returns an int
+ * @i: carries int
+ */
+
+int main(int i)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (i);
+}
