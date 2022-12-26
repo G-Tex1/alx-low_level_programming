0b6f354aa869ff4137553b5902f02b430b01707d
G-Tex1 <ghenorbari@gmail.com>
2022-12-26 07:50:06 -0800

    num

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints number combination
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 153; num++)
+	{
+		if (num < 153 && num != 11 ||
+				num != 22 ||
+				num != 33 ||
+				num != 44 ||
+				num != 55 ||
+				num != 66 ||
+				num != 77 ||
+				num != 88 ||
+				num != 99)
+		{
+			putchar((num % 16) + '0');
+			putchar(',');
+			putchar('\n');
+		}
+		for (num = 153; num <= 153; num++)
+		{
+			putchar((num % 16) + '0');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
