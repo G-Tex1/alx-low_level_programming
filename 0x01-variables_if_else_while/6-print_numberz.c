cc29d050a508440777cc5ed3bde6f885398a5c98
G-Tex1 <ghenorbari@gmail.com>
2022-12-26 03:29:04 -0800

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers of base ten
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
