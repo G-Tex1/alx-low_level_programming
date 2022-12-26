0c947c280205f6efdc64e8e54c6e67233f9ee6ae
G-Tex1 <ghenorbari@gmail.com>
2022-12-26 05:51:04 -0800

    num

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	for (num = 9; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-	}
+	putchar('\n');
 	return (0);
 }
