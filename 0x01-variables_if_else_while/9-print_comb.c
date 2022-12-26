54d983b99b3811a07607e7ce629210c3e7c027c8
G-Tex1 <ghenorbari@gmail.com>
2022-12-26 05:01:23 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
