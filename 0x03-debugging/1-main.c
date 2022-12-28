904f76692a8e46a1186b5c7de4da3b89b39bde19
G-Tex1 <ghenorbari@gmail.com>
2022-12-28 10:58:38 -0800

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,11 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+
+	//while (i < 10)
+	//{
+	//	putchar(i);
+	//}
 
 	printf("Infinite loop avoided! \\o/\n");
 
