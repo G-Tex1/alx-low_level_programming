e1075fc603c919946cafead87e4c3498e3a2a227
G-Tex1 <ghenorbari@gmail.com>
2022-12-29 09:16:00 -0800

    bet

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (a > c)
-		{
 		largest = a;
-		}
 	}
-	else if (b > a)
+	else if (b > a && b > c)
 	{
-		if (b > c)
-		{
 		largest = b;
-		}
 	}
 	else
 	{
