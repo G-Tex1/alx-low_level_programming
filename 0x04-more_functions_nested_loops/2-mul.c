86304652b178b9398bcb59644fe4a71284e06ebd
G-Tex1 <ghenorbari@gmail.com>
2022-12-30 06:03:47 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply integers
+ * @a: integer a
+ * @b: integer b
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul == a * b;
+	return (mul);
+}
