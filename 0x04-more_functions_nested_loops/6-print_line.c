81d68ed6aca08d94085d32c2d53275e161a23f0e
G-Tex1 <ghenorbari@gmail.com>
2023-01-03 14:51:27 -0800

    execute

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,12 @@
 
 void print_line(int n)
 {
-	int j;
+	int line;
 
 	if (n > 0)
 	{
-		for (j = 0; j <= n; j++)
-		{
+		for (line = 0; line < n; line++)
 			_putchar('_');
-		}
-		_putchar('\n');
 	}
-	else if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
