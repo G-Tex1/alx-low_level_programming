7ad8a2eb89a833fac989bc751ffd87fe59312434
G-Tex1 <ghenorbari@gmail.com>
2023-01-04 12:22:36 -0800

    betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s[i] != '\0')
-	{
-		_putchar(s[i] % 100);
-	}
-	_strlen_recursion(*s + 1);
-	return (*s[i]);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
