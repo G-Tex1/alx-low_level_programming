6bc18eeba98b41dbe10b1339c3169309674e354d
G-Tex1 <ghenorbari@gmail.com>
2023-01-04 07:53:48 -0800

    len

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s: character input
+ * Return: return result
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		_putchar((s[i] % 10) != '\0');
+		_strlen_recursion(*s + 1);
+	}
+	return (s[i]);
+}
