63535c1d7782cd2f053f09353965659869c33ee0
G-Tex1 <ghenorbari@gmail.com>
2023-01-11 11:18:40 -0800

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * @argv: argument
+ * @argc: argument count
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
